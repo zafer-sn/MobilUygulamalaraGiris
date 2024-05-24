4dcde4aa919e3639623670d34aa90b0a5eead072
zafer-sn <162111813+zafer-sn@users.noreply.github.com>
2024-05-24 09:45:22 +0300

    uyg

---
+++ lib/YapilacaklarSablon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YapilacaklarSablon extends StatelessWidget {
+  const YapilacaklarSablon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
